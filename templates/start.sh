efc7df0fefffc2388780134a64faa4c26be08016
J. David Ibáñez <jdavid.ibp@gmail.com>
2017-04-01 12:36:38 +0200

    Define env variables in the start script

--- templates/start.sh
+++ templates/start.sh
@@ -1,4 +1,7 @@
 {% if django_prefix %}
 export PATH="{{ django_prefix }}/bin:$PATH"
 {% endif %}
+{% for var in env_vars %}
+export {{ var.name }}="{{ var.value }}"
+{% endfor %}
 uwsgi {{ dir_etc }}/uwsgi.ini
