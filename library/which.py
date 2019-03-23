1c7e974f6788d49401f3730ee0dbca63103dd74d
J. David Ibáñez <jdavid.ibp@gmail.com>
2019-03-23 09:11:15 +0100

    Clean find out of paths to binaries

--- library/which.py
+++ library/which.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python
 
-
+import os
 from ansible.module_utils.basic import AnsibleModule
 
 
 def main():
     module = AnsibleModule(
         argument_spec = dict(
+            base=dict(),
             name=dict(required=True),
             dest=dict(),
         )
     )
 
+    base = module.params['base']
     name = module.params['name']
     dest = module.params['dest']
     if dest is None:
         dest = name
 
-    rc, out, err = module.run_command(['which', name])
-    out = out.strip()
+    if base is None:
+        # Call which
+        rc, out, err = module.run_command(['which', name])
+        out = out.strip()
+    else:
+        # Use base directory directly
+        out = os.path.join(base, name)
 
+    # Ok
     module.exit_json(changed=False, ansible_facts={dest: out})
 
 
