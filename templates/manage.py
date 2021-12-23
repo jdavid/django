1152f56249880f8cce07b40795faaf084fee3c34
J. David Ibáñez <jdavid.ibp@gmail.com>
2021-12-23 11:33:44 +0100

    Support extra PYTHONPATH

--- templates/manage.py
+++ templates/manage.py
@@ -4,6 +4,10 @@ import os
 import sys
 
 
+{% if pythonpath %}
+sys.path.append("{{ pythonpath }}")
+{% endif %}
+
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{ settings }}')
