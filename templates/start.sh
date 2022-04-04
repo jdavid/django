add9ce26703f5b70c4ece1cad7ad34ed702eb5c3
J. David Ibáñez <jdavid.ibp@gmail.com>
2022-04-04 19:16:13 +0200

    Remove unused env_vars
    Also update pip, and replace app_path by project_path in django_manage.

--- templates/start.sh
+++ templates/start.sh
@@ -1,7 +1,4 @@
 {% if prefix %}
 export PATH="{{ prefix }}/bin:$PATH"
 {% endif %}
-{% for var in env_vars %}
-export {{ var.name }}="{{ var.value }}"
-{% endfor %}
 {{ dir_bin }}/gunicorn -c {{ dir_etc }}/gunicorn.conf.py
