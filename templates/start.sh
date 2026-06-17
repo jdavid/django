b337ded41631cc3c187421ff59bf1acfc7d3d756
J. David Ibáñez <jdavid.ibp@gmail.com>
2026-06-17 08:31:38 +0200

    Rotate logs on reload/restart

--- templates/start.sh
+++ templates/start.sh
@@ -1,4 +1,6 @@
 {% if prefix %}
 export PATH="{{ prefix }}/bin:$PATH"
 {% endif %}
+{{ django_root }}/ansible/rotate-logs.sh || true
 {{ dir_bin }}/supervisord -c {{ dir_etc }}/supervisor.conf
+{{ django_root }}/ansible/rotate-logs.sh --compress || true
