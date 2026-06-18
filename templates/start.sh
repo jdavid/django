a25577f0b6bb2a360536ab5f5b03496efc32bd9a
J. David Ibáñez <jdavid.ibp@gmail.com>
2026-06-18 16:13:26 +0200

    Remove custom log rotation

--- templates/start.sh
+++ templates/start.sh
@@ -1,6 +1,4 @@
 {% if prefix %}
 export PATH="{{ prefix }}/bin:$PATH"
 {% endif %}
-{{ django_root }}/ansible/rotate-logs.sh || true
 {{ dir_bin }}/supervisord -c {{ dir_etc }}/supervisor.conf
-{{ django_root }}/ansible/rotate-logs.sh --compress || true
